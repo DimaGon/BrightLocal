0f375b5e5296d32f01006db648882aa04594cafd
dmitry_honcharuk <dmitry.honcharuk@gmail.com>
2017-02-18 17:39:37 +0200

    added navigation block with menu

--- client/app.js
+++ client/app.js
@@ -3,5 +3,7 @@ import "./common/_global.scss";
 import "./components/container/container.scss";
 import "./components/header/header.scss";
 import "./components/logo/logo.scss";
+import "./components/navigation/navigation.scss";
+import "./components/menu/menu.scss";
 
 //Import Components
