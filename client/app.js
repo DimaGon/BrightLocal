4ebdf0e0e7fbc7a74781820ce6ece96798ae74cf
dmitry_honcharuk <dmitry.honcharuk@gmail.com>
2017-02-18 14:35:40 +0200

    added styles for logo

--- client/app.js
+++ client/app.js
@@ -1,8 +1,7 @@
 "use strict";
-//Import Styles
 import "./common/_global.scss";
-
 import "./components/container/container.scss";
 import "./components/header/header.scss";
+import "./components/logo/logo.scss";
 
 //Import Components
