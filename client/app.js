c2c39513862521e4377ae8b4ba2b2468d21ce65e
dmitry_honcharuk <dmitry.honcharuk@gmail.com>
2017-02-18 19:00:17 +0200

    added breadcrumbs

--- client/app.js
+++ client/app.js
@@ -8,6 +8,7 @@ import "./components/logo/logo.scss";
 import "./components/navigation/navigation.scss";
 import "./components/menu/menu.scss";
 import "./components/footer/footer.scss";
+import "./components/breadcrumbs/breadcrumbs.scss";
 
 // Scripts
 import "./components/navigation/navigation";
